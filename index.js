2ebb86780c2e4e61465ce411c022e8e249a8c637
Terence Sam <sam.p.terence@gmail.com>
2022-06-29 14:54:43 -0600

    df

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-const bigButton = document.querySelector('#meanOldDroid')
+const bigButton = document.querySelector('#iNeedWeapon')
 
 const message = () => {
     alert('Well come right this way!')
 }
 
 bigButton.addEventListener('click', message)
+
+
+
+// const bigButtonTwo = document.querySelector('#iNeedWeapon2')
+
+// const message = () => {
+//     alert('Well come right this way!')
+// }
+
+// bigButton.addEventListener('click', message)
+
+
+
+// const bigButtonThree = document.querySelector('#iNeedWeapon3')
+
+// const message = () => {
+//     alert('Well come right this way!')
+// }
+
+// bigButton.addEventListener('click', message)
